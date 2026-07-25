87f8c82c92d5223f46b8f404e93c0e845ecca461
simon carmona toro <simoncarmonat@hotmail.com>
2026-07-25 03:47:24 -0500

    Guardar cambios finales en el código

---
+++ src/condicionales/condicional.py
@@ -0,0 +1,5 @@
+edad = 20
+if edad >= 18:
+    print("Mayor de edad")
+else:
+    print("Menor de edad")
---
+++ src/funciones/funcion.py
@@ -0,0 +1,5 @@
+def calcular_area_rectangulo(base, altura):
+    return base * altura
+
+area = calcular_area_rectangulo(10, 5)
+print(area)
